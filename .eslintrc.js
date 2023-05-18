d463fac30c76b28d27e6a0b72799b1e9e155237e
Yaro <jaroba0@gmail.com>
2023-05-18 16:06:44 +0200

    eshop  16 diel

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+/* eslint-disable quote-props */
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2020: true,
+  },
+  extends: ['airbnb-base',"prettier" ],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 11,
+  },
+  rules: {
+    "no-console": 0,
+    "linebreak-style": ["error", "windows"],
+    "quotes": "off",
+    "no-nested-ternary": 0,
+    "import/prefer-default-export" :0,
+
+  },
+};
